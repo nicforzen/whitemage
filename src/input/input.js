25980166825015e8a1807e57219475adaf03f08d
Nic Wilson <88619060+nicforzen@users.noreply.github.com>
2021-09-16 00:46:06 -0700

    Inidividualize prototypes in input.js

--- src/input/input.js
+++ src/input/input.js
@@ -4,114 +4,112 @@ export function Input() {
     this.instance = null;
 }
 
-Input.prototype = {
-    setInstance(instance){
-        this.instance = instance;
-    },
-    onKeyDown(e){
-        var key = String.fromCharCode(e.keyCode);
-        if(this.keysDown.indexOf(key) < 0){
-            this.keysDown.push(key);
-        }
-    },
-    onKeyUp(e){
-        var key = String.fromCharCode(e.keyCode);
-        let index = this.keysDown.indexOf(key);
-        if(index > -1) this.keysDown.splice(index, 1);
-        if (this.keysUp.indexOf(key) < 0) {
-            this.keysUp.push(key);
-        }
-    },
-    mouseDown(e){
-        var local = this.instance.render._getRawCursorPosition(e);
-        var point = { x: local.x, y: local.y, button: e.which};
-        for(let i=0;i<this.instance.p_uiItems.length;i++){
-            let gameObj = this.instance.p_uiItems[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseDown
-                    && point.x != null && point.y != null){
-                        let handled = script.onMouseDown(point);
-                        if(handled) return;
-                    }
-            }
-        }
-        local = this.instance.render._getCursorPosition(e);
-        point = { x: local.x, y: local.y, button: e.which};
-        for(let i=0;i<this.instance.p_gameObjects.length;i++){
-            let gameObj = this.instance.p_gameObjects[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseDown
-                    && point.x != null && point.y != null) script.onMouseDown(point);
-            }
-        }
-    },
-    mouseUp(e){
-        e.preventDefault();
-        var local = this.instance.render._getRawCursorPosition(e);
-        var point = { x: local.x, y: local.y, button: e.which };
-        for(let i=0;i<this.instance.p_uiItems.length;i++){
-            let gameObj = this.instance.p_uiItems[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseUp
-                    && point.x != null && point.y != null) {
-                        let handled = script.onMouseUp(point);
-                        if(handled) return;
-                    }
-            }
-        }
-        local = this.instance.render._getCursorPosition(e);
-        point = { x: local.x, y: local.y, button: e.which};
-        for(let i=0;i<this.instance.p_gameObjects.length;i++){
-            let gameObj = this.instance.p_gameObjects[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseUp
-                    && point.x != null && point.y != null) script.onMouseUp(point);
-            }
-        }
-    },
-    mouseMove(e){
-        var local = this.instance.render._getRawCursorPosition(e);
-        if(local.x < 0 || local.y < 0) return;
-        var point = { x: local.x, y: local.y, button: e.which };
-        for(let i=0;i<this.instance.p_uiItems.length;i++){
-            let gameObj = this.instance.p_uiItems[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseMove
-                    && point.x != null && point.y != null) script.onMouseMove(point);
-            }
-        }
-        local = this.instance.render._getCursorPosition(e);
-        point = { x: local.x, y: local.y, button: e.which};
-        for(let i=0;i<this.instance.p_gameObjects.length;i++){
-            let gameObj = this.instance.p_gameObjects[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onMouseMove
-                    && point.x != null && point.y != null) script.onMouseMove(point);
-            }
-        }
-    },
-    scroll(delta){
-        for(let i=0;i<this.instance.p_gameObjects.length;i++){
-            let gameObj = this.instance.p_gameObjects[i];
-            for(let j=0;j<gameObj.scripts.length;j++){
-                let script = gameObj.scripts[j];
-                if(script.onScroll) script.onScroll(delta);
-            }
-        }
-    },
-    isDown(name){
-        return this.keysDown.indexOf(name) >= 0;
-    },
-    isUp(name){
-        return this.keysUp.indexOf(name) >= 0;
-    },
-    p_clearUpKeys(){
-        this.keysUp = [];
+Input.prototype.setInstance = function(instance){
+    this.instance = instance;
+}
+Input.prototype.onKeyDown(e){
+    var key = String.fromCharCode(e.keyCode);
+    if(this.keysDown.indexOf(key) < 0){
+        this.keysDown.push(key);
     }
-};
+}
+Input.prototype.onKeyUp = function(e){
+    var key = String.fromCharCode(e.keyCode);
+    let index = this.keysDown.indexOf(key);
+    if(index > -1) this.keysDown.splice(index, 1);
+    if (this.keysUp.indexOf(key) < 0) {
+        this.keysUp.push(key);
+    }
+}
+Input.prototype.mouseDown = function(e){
+    var local = this.instance.render._getRawCursorPosition(e);
+    var point = { x: local.x, y: local.y, button: e.which};
+    for(let i=0;i<this.instance.p_uiItems.length;i++){
+        let gameObj = this.instance.p_uiItems[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseDown
+                && point.x != null && point.y != null){
+                    let handled = script.onMouseDown(point);
+                    if(handled) return;
+                }
+        }
+    }
+    local = this.instance.render._getCursorPosition(e);
+    point = { x: local.x, y: local.y, button: e.which};
+    for(let i=0;i<this.instance.p_gameObjects.length;i++){
+        let gameObj = this.instance.p_gameObjects[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseDown
+                && point.x != null && point.y != null) script.onMouseDown(point);
+        }
+    }
+}
+Input.prototype.mouseUp = function(e){
+    e.preventDefault();
+    var local = this.instance.render._getRawCursorPosition(e);
+    var point = { x: local.x, y: local.y, button: e.which };
+    for(let i=0;i<this.instance.p_uiItems.length;i++){
+        let gameObj = this.instance.p_uiItems[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseUp
+                && point.x != null && point.y != null) {
+                    let handled = script.onMouseUp(point);
+                    if(handled) return;
+                }
+        }
+    }
+    local = this.instance.render._getCursorPosition(e);
+    point = { x: local.x, y: local.y, button: e.which};
+    for(let i=0;i<this.instance.p_gameObjects.length;i++){
+        let gameObj = this.instance.p_gameObjects[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseUp
+                && point.x != null && point.y != null) script.onMouseUp(point);
+        }
+    }
+}
+Input.prototype.mouseMove = function(e){
+    var local = this.instance.render._getRawCursorPosition(e);
+    if(local.x < 0 || local.y < 0) return;
+    var point = { x: local.x, y: local.y, button: e.which };
+    for(let i=0;i<this.instance.p_uiItems.length;i++){
+        let gameObj = this.instance.p_uiItems[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseMove
+                && point.x != null && point.y != null) script.onMouseMove(point);
+        }
+    }
+    local = this.instance.render._getCursorPosition(e);
+    point = { x: local.x, y: local.y, button: e.which};
+    for(let i=0;i<this.instance.p_gameObjects.length;i++){
+        let gameObj = this.instance.p_gameObjects[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onMouseMove
+                && point.x != null && point.y != null) script.onMouseMove(point);
+        }
+    }
+}
+Input.prototype.scroll = function(delta){
+    for(let i=0;i<this.instance.p_gameObjects.length;i++){
+        let gameObj = this.instance.p_gameObjects[i];
+        for(let j=0;j<gameObj.scripts.length;j++){
+            let script = gameObj.scripts[j];
+            if(script.onScroll) script.onScroll(delta);
+        }
+    }
+}
+Input.prototype.isDown = function(name){
+    return this.keysDown.indexOf(name) >= 0;
+}
+Input.prototype.isUp = function(name){
+    return this.keysUp.indexOf(name) >= 0;
+}
+Input.prototype.p_clearUpKeys = function(){
+    this.keysUp = [];
+}

dc6f4e996b844d26f3dc039ccc08e169bf0c9315
Nic Wilson <88619060+nicforzen@users.noreply.github.com>
2021-09-16 02:17:40 -0700

    Add more fields to scene.js

--- src/control/scene.js
+++ src/control/scene.js
@@ -3,8 +3,12 @@ export function Scene(instance, onStart, onRender) {
     this.instance = instance;
     this.onStart = onStart;
     this.onRender = onRender;
+
+    this.isServer = false;
+    this.initialized = false;
 }
 
 Scene.prototype.setInstance = function(instance){
+    this.isServer = instance.isServer;
     this.instance = instance;
 };
